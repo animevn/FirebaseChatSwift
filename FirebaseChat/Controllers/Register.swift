27ddfa0d19f7451ea727d2d26022ffb096c6f4fb
animevn <haanhth21@gmail.com>
2020-02-10 12:06:28 +0700

    add register

--- FirebaseChat/Controllers/Register.swift
+++ FirebaseChat/Controllers/Register.swift
@@ -5,4 +5,7 @@ class RegisterController: UIViewController {
     @IBOutlet weak var tfEmail: UITextField!
     @IBOutlet weak var tfPassword: UITextField!
     
+    @IBAction func onButtonRegisterPressed(_ sender: UIButton) {
+    }
+    
 }
