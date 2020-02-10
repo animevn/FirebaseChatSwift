e3be59ec14cc224d75b996e9674d2a10d46c34a2
animevn <haanhth21@gmail.com>
2020-02-10 12:25:59 +0700

    update layouts

--- FirebaseChat/Controllers/Login.swift
+++ FirebaseChat/Controllers/Login.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class LoginController:UIViewController{
     
@@ -6,6 +7,18 @@ class LoginController:UIViewController{
     @IBOutlet weak var tfPassword: UITextField!
     
     @IBAction func onButtonLoginPressed(_ sender: UIButton) {
+        guard let email = tfEmail.text, let password = tfPassword.text,
+            !email.isEmpty, !password.isEmpty else {return}
+       
+        Auth.auth().signIn(withEmail: email, password: password){[weak self] result, error in
+            guard let strongSelf = self else {return}
+            if let error = error{
+                print(error.localizedDescription)
+            }else{
+                strongSelf.performSegue(withIdentifier: Base.segueLoginChat, sender: strongSelf)
+            }
+        }
+        
     }
     
     
--- FirebaseChat/Controllers/Register.swift
+++ FirebaseChat/Controllers/Register.swift
@@ -1,4 +1,5 @@
 import UIKit
+import Firebase
 
 class RegisterController: UIViewController {
     
@@ -6,6 +7,16 @@ class RegisterController: UIViewController {
     @IBOutlet weak var tfPassword: UITextField!
     
     @IBAction func onButtonRegisterPressed(_ sender: UIButton) {
+        guard let email = tfEmail.text, let password = tfPassword.text,
+            !email.isEmpty, !password.isEmpty else {return}
+        Auth.auth().createUser(withEmail: email, password: password){[weak self] result, error in
+            guard let strongSelf = self else {return}
+            if let error = error{
+                print(error.localizedDescription)
+            }else{
+                strongSelf.performSegue(withIdentifier: Base.segueRegisterChat, sender: strongSelf)
+            }
+        }
     }
     
 }
